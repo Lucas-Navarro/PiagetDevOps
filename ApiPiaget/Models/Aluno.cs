6110a0991f89b2147191e251cfd1baa5602b2d62
Lucas-Navarro <lucasr6out@gmail.com>
2025-03-11 10:54:26 -0300

    feat: Adicionado controle de Aluno

--- ApiPiaget/Models/Aluno.cs
+++ ApiPiaget/Models/Aluno.cs
@@ -4,7 +4,7 @@
     {
         // Definindo os atributos do objeto Aluno
         public int Id { get; set; }
-        // ? deixa o ponto luno
+        // ? deixa o ponto para poder ser vazio
         public string? Nome { get; set; }
         public string? RGM { get; set; }
         public decimal Nota { get; set; }
