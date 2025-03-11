8aeca6e1f41c6d3e088fdc47084e963cfe083ffd
Lucas-Navarro <lucasr6out@gmail.com>
2025-03-11 09:40:03 -0300

    feat: Projeto DevOps

---
+++ ApiPiaget/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiPiaget.Models
+{
+    public class Aluno
+    {
+        // Definindo os atributos do objeto Aluno
+        public int Id { get; set; }
+        // ? deixa o ponto luno
+        public string? Nome { get; set; }
+        public string? RGM { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
